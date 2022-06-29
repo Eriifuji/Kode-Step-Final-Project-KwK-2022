fdbfb1b52d0b6f73e8daad58a8f7e76d868560e4
Rania <raniasaleh729@gmail.com>
2022-06-29 11:58:55 -0400

    storyboard

---
+++ Kode Step Final Project KwK 2022/ChooseSubjectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChooseSubjectViewController.swift
+//  Kode Step Final Project KwK 2022
+//
+//  Created by Rania Saleh on 6/29/22.
+//
+
+import UIKit
+
+class ChooseSubjectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
