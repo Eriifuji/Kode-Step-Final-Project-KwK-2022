1ed4970a6e47557a106c0d09e4863854fe7b8c9c
Eriifuji <erifujita29@icloud.com>
2022-06-29 16:00:38 -0300

    New page for Arrays

---
+++ Kode Step Final Project KwK 2022/ArrayInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ArrayInfoViewController.swift
+//  Kode Step Final Project KwK 2022
+//
+//  Created by Eri Fujita on 2022-06-29.
+//
+
+import UIKit
+
+class ArrayInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Kode Step Final Project KwK 2022/HomeViewController.swift
+++ Kode Step Final Project KwK 2022/HomeViewController.swift
@@ -1,20 +1,22 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Kode Step Final Project KwK 2022
 //
-//  Created by Eri Fujita on 2022-06-28.
+//  Created by Eri Fujita on 2022-06-29.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
     
+
+  
     
 
 }
-
