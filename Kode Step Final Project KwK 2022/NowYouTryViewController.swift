ea870866d7eded5527525484a111698c14c73a2f
Tiffany Cai <tiffanycai.vv@gmail.com>
2022-06-30 18:59:25 -0400

    code added

--- Kode Step Final Project KwK 2022/NowYouTryViewController.swift
+++ Kode Step Final Project KwK 2022/NowYouTryViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 class NowYouTryViewController: UIViewController {
 
     @IBOutlet weak var answerArray: UILabel!
+    @IBOutlet weak var correct: UILabel!
+    @IBOutlet weak var yes: UIButton!
+    @IBOutlet weak var no: UIButton!
+    @IBOutlet weak var woohoo: UILabel!
+    @IBOutlet weak var wrong: UILabel!
+    @IBOutlet weak var n: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        answerArray.isHidden = true
+        correct.isHidden = true
+        yes.isHidden = true
+        no.isHidden = true
+        n.isHidden = true
+        woohoo.isHidden = true
+        wrong.isHidden = true
 
-        // Do any additional setup after loading the view.
+    }
+    @IBAction func done(_ sender: Any) {
+        answerArray.isHidden = false
+        n.isHidden = false
+    }
+    @IBAction func n(_ sender: Any) {
+        correct.isHidden = false
+        yes.isHidden = false
+        no.isHidden = false
+    }
+    @IBAction func yes(_ sender: Any) {
+        woohoo.isHidden = false
+        wrong.isHidden = true
+    }
+    @IBAction func no(_ sender: Any) {
+        woohoo.isHidden = true
+        wrong.isHidden = false
     }
     
 
