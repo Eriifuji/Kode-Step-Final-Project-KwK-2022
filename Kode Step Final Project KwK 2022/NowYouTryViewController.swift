fa46831e64ceee3f7f7adf3c7e585b89c6761c04
Rania <raniasaleh729@gmail.com>
2022-06-30 18:32:04 -0400

    interactive screen

---
+++ Kode Step Final Project KwK 2022/NowYouTryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NowYouTryViewController.swift
+//  Kode Step Final Project KwK 2022
+//
+//  Created by Rania Saleh on 6/30/22.
+//
+
+import UIKit
+
+class NowYouTryViewController: UIViewController {
+
+    @IBOutlet weak var answerArray: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
